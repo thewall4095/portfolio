c3139af244ee21fa6f18605ff4746a8b826d8a7f
Dhawal Patel <dhawalbhai.patel@capitalfloat.com>
2019-10-23 20:12:27 +0530

    Rectified path in server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();  
 app.use(express.static(__dirname + '/dist/portfolio'));  
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/portfolio/index.html');  
 });  
 app.listen(process.env.PORT || 8080); 
